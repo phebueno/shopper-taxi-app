092447a526917fdf4e710fa26a7b16224ac843d2
Fernando Bueno <fcbgomes@gmail.com>
2024-11-23 01:41:13 -0300

    feat: added Prisma startup configuration along with Dockerfile and docker-compose envs setup

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('users')
+  async getPublishedPosts(): Promise<User[]> {
+    return this.userService.users({})
   }
 }
