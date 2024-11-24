42713676ba9578382866a9dc97e0e53e4465246a
Fernando Bueno <fcbgomes@gmail.com>
2024-11-23 21:17:19 -0300

    feat: added generic data validation error to match expected error object

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv'
+import { config } from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-config({ path: '../.env' })
+import { validationPipeOptions } from './common/filters/validation.filter';
+config({ path: '../.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe(validationPipeOptions),
+  );
+  
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
--- backend/src/rides/dto/create-ride.dto.ts
+++ backend/src/rides/dto/create-ride.dto.ts
@@ -2,18 +2,18 @@ import { Transform, TransformFnParams } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateRideDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Customer ID must be a string' })
+  @IsNotEmpty({ message: 'Customer ID should not be empty' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   customer_id: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Origin must be a string' })
+  @IsNotEmpty({ message: 'Origin should not be empty' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   origin: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Destination must be a string' })
+  @IsNotEmpty({ message: 'Destination should not be empty' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   destination: string;
 }
