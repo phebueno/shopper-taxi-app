55bf324ba8f89ec3dc3fd9b8534aa3095c60d39f
Fernando Bueno <fcbgomes@gmail.com>
2024-11-25 02:51:12 -0300

    fix: added cors policy to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ config({ path: '../.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
 
   await app.listen(process.env.PORT ?? 8080);
