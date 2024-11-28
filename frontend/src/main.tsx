cfa6fe18f685def8252ec900478a82e589f79fb3
Fernando Bueno <fcbgomes@gmail.com>
2024-11-27 22:56:35 -0300

    chore: removed strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@/index.css'
-import App from '@/App.tsx'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/index.css";
+import App from "@/App.tsx";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+createRoot(document.getElementById("root")!).render(
+  <ChakraProvider value={defaultSystem}>
     <App />
-    </ChakraProvider>
-  </StrictMode>,
-)
+  </ChakraProvider>
+);
